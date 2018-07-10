2beda3b8b0d4ecb3b8d6b81ab12caab7881449f8
DESKTOP-RB5MSRC\清華 <matsuosayaka@gmail.com>
2018-07-10 18:17:47 +0900

    Q2

---
+++ Q2.sql
@@ -0,0 +1,3 @@
+CREATE TABLE item(item_id int PRIMARY KEY AUTO_INCREMENT,item_name VARCHAR(256) NOT NULL,item_price int NOT NULL,category_id int);
+
+
---
+++ Q3.sql
@@ -0,0 +1,6 @@
+CREATE TABLE item_category(category_id int PRIMARY KEY AUTO_INCREMENT,category_name VARCHAR(256) NOT NULL);
+INSERT INTO item_category VALUES(1,'â∆ãÔ');
+INSERT INTO item_category VALUES(2,'êHïi');
+INSERT INTO item_category VALUES(3,'ñ{');
+
+
